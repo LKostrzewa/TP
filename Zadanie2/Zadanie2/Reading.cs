040d262445f63bd33b088dfa9476f943244eb6c1
Łukasz Kostrzewa <lukko98@gmail.com>
2019-11-13 11:56:31 +0100

    Pierwsza koncepcja serializacji własnej na przykładzie katalogu (działa)

---
+++ Zadanie2/Zadanie2/Reading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadanie1;
+
+namespace Zadanie2
+{
+    public static class Reading
+    {
+        public static Katalog ReadKatalogFromFile(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            string[] parameters = reader.ReadLine().Split(';');
+            return new Katalog(Int32.Parse(parameters[0]), parameters[1], parameters[2], Int32.Parse(parameters[3]));
+        }
+    }
+}
---
+++ Zadanie2/Zadanie2Test/SerializationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie2;
+using Zadanie1;
+
+namespace Zadanie2Test
+{
+    [TestClass]
+    public class SerializationTest
+    {
+        [TestMethod]
+        public void WriteKatalogToFileTest()
+        {
+            Katalog kat = new Katalog(0, "Programowanie c#", "Podrecznik", 520);
+            Writing.WriteKatalogToFile(kat, "test.txt");
+
+            Katalog kat2 = Reading.ReadKatalogFromFile("test.txt");
+
+            Console.WriteLine(kat2);
+            Assert.AreEqual<Katalog>(kat, kat2);
+        }
+    }
+}
