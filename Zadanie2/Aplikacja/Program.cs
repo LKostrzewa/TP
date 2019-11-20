5e675d464e6cd2ca1e8bcb5406df1874640cab9b
Łukasz Kostrzewa <lukko98@gmail.com>
2019-11-20 21:27:17 +0100

    Poprawa struktury programu

--- Zadanie2/Aplikacja/Program.cs
+++ Zadanie2/Aplikacja/Program.cs
@@ -17,15 +17,13 @@ namespace Aplikacja
             DataRepository dr = new DataRepository(new WypelnianieStalymi());
             Reading reading = new Reading();
             ObjectIDGenerator iDGenerator = new ObjectIDGenerator();
-            string type = "1";
+            Console.WriteLine("Jaka czynnosc chcesz wykonac\n1) zapis do pliku (txt)\n2) odczyt z pliku (txt)\n3) zapis do pliku (json)\n4) odczyt z pliku (json)\n\nKazdy inny znak zakonczy dzialanie programu");
+            string type = Console.ReadLine();
+            Console.WriteLine(type);
             while (type == "1" | type == "2" | type == "3" | type == "4")
             {
-                Console.WriteLine("Jaka czynnosc chcesz wykonac\n1) zapis do pliku (txt)\n2) odczyt z pliku (txt)\n3) zapis do pliku (json)\n4) odczyt z pliku (json)\n\nKazdy inny znak zakonczy dzialanie programu");
-                type = Console.ReadLine();
-                Console.WriteLine(type);
                 Console.WriteLine("Podaj nazwe pliku : ");
                 string path = Console.ReadLine();
-
                 switch (type)
                 {
                     case "1":
@@ -49,28 +47,31 @@ namespace Aplikacja
                         dr.SetDataContext(dc);
                         break;
                     case "3":
-                        //Writing.WriteCollectionToJSON<Katalog>(dr.GetAllKatalog(), path + "1.json");
-                        // Writing.WriteCollectionToJSON<OpisStanu>(dr.GetAllOpisStanu(), path + "2.json");
-                        //Writing.WriteCollectionToJSON<Wykaz>(dr.GetAllWykaz(), path + "3.json");
-                        //Writing.WriteCollectionToJSON<Zdarzenie>(dr.GetAllZdarzenie(), path + "4.json");
-                        Writing.WriteObjectToJSON(dr.GetDataContext(), path + ".json");
+                        Writing.WriteCollectionToJSON<Katalog>(dr.GetAllKatalog(), path + "1.json");
+                        Writing.WriteCollectionToJSON<OpisStanu>(dr.GetAllOpisStanu(), path + "2.json");
+                        Writing.WriteCollectionToJSON<Wykaz>(dr.GetAllWykaz(), path + "3.json");
+                        Writing.WriteCollectionToJSON<Zdarzenie>(dr.GetAllZdarzenie(), path + "4.json");
+                        //Writing.WriteObjectToJSON(dr.GetDataContext(), path + ".json");
                         break;
                     case "4":
-                        //List<Katalog> katalogsJSON = (List<Katalog>)Reading.ReadCollectionFromJSON<Katalog>(path + "1.json");
-                        //List<OpisStanu> opisStanusJSON = (List<OpisStanu>)Reading.ReadCollectionFromJSON<OpisStanu>(path + "2.json");
-                        //List<Wykaz> wykazsJSON = (List<Wykaz>)Reading.ReadCollectionFromJSON<Wykaz>(path + "3.json");
-                        //List<Zdarzenie> zdarzeniesJSON = (List<Zdarzenie>)Reading.ReadCollectionFromJSON<Zdarzenie>(path + "4.json");
-                        //DataContext dcJSON = new DataContext
-                        //{
-                        //    opisyStanu = opisStanusJSON,
-                        //    wykazy = wykazsJSON,
-                        //    zdarzenia = new ObservableCollection<Zdarzenie>(zdarzeniesJSON),
-                        //    katalogi = katalogsJSON.ToDictionary(k => k.id, k => k)
-                        //};
-                        //dr.SetDataContext(dcJSON);
-                        dr.SetDataContext(Reading.ReadObjectFromJSON<DataContext>(path + ".json"));
+                        List<Katalog> katalogsJSON = (List<Katalog>)Reading.ReadCollectionFromJSON<Katalog>(path + "1.json");
+                        List<OpisStanu> opisStanusJSON = (List<OpisStanu>)Reading.ReadCollectionFromJSON<OpisStanu>(path + "2.json");
+                        List<Wykaz> wykazsJSON = (List<Wykaz>)Reading.ReadCollectionFromJSON<Wykaz>(path + "3.json");
+                        List<Zdarzenie> zdarzeniesJSON = (List<Zdarzenie>)Reading.ReadCollectionFromJSON<Zdarzenie>(path + "4.json");
+                        DataContext dcJSON = new DataContext
+                        {
+                            opisyStanu = opisStanusJSON,
+                            wykazy = wykazsJSON,
+                            zdarzenia = new ObservableCollection<Zdarzenie>(zdarzeniesJSON),
+                            katalogi = katalogsJSON.ToDictionary(k => k.id, k => k)
+                        };
+                        dr.SetDataContext(dcJSON);
+                        //dr.SetDataContext(Reading.ReadObjectFromJSON<DataContext>(path + ".json"));
                         break;
                 }
+                Console.WriteLine("Jaka czynnosc chcesz wykonac\n1) zapis do pliku (txt)\n2) odczyt z pliku (txt)\n3) zapis do pliku (json)\n4) odczyt z pliku (json)\n\nKazdy inny znak zakonczy dzialanie programu");
+                type = Console.ReadLine();
+                Console.WriteLine(type);
             }
         }
     }
