45722bf9124be2572ec1a93f08434a1b6a8d9f23
Paweł Białek <216723@edu.p.lodz.pl>
2019-11-20 19:52:09 +0100

    Dodanie programu konsolowego do zapisu/odczytu formatu JSON

--- Zadanie2/Aplikacja/Program.cs
+++ Zadanie2/Aplikacja/Program.cs
@@ -17,35 +17,58 @@ namespace Aplikacja
             DataRepository dr = new DataRepository(new WypelnianieStalymi());
             Reading reading = new Reading();
             ObjectIDGenerator iDGenerator = new ObjectIDGenerator();
-
-            Console.WriteLine("Jaka czynnosc chcesz wykonac\n1) zapis do pliku\n2) odczyt z pliku");
-            string type = Console.ReadLine();
-            Console.WriteLine(type);
-            Console.WriteLine("Podaj nazwe pliku : ");
-            string path = Console.ReadLine();
-
-            switch (type)
+            string type = "1";
+            while (type == "1" | type == "2" | type == "3" | type == "4")
             {
-                case "1":
-                    Writing.WriteKatalogsToFile(dr.GetAllKatalog(), path + "1.txt", iDGenerator);
-                    Writing.WriteOpisStanusToFile(dr.GetAllOpisStanu(), path + "2.txt", iDGenerator);
-                    Writing.WriteWykazsToFile(dr.GetAllWykaz(), path + "3.txt", iDGenerator);
-                    Writing.WriteZdarzeniesToFile(dr.GetAllZdarzenie(), path + "4.txt", iDGenerator);
-                    break;
-                case "2":
-                    List<Katalog> katalogs = (List<Katalog>)reading.ReadKatalogsFromFile(path + "1.txt");
-                    List<OpisStanu> opisStanus = (List<OpisStanu>)reading.ReadOpisStanusFromFile(path + "2.txt");
-                    List<Wykaz> wykazs = (List<Wykaz>)reading.ReadWykazsFromFile(path + "3.txt");
-                    List<Zdarzenie> zdarzenies = (List<Zdarzenie>)reading.ReadZdarzeniesFromFile(path + "4.txt");
-                    DataContext dc = new DataContext
-                    {
-                        opisyStanu = opisStanus,
-                        wykazy = wykazs,
-                        zdarzenia = new ObservableCollection<Zdarzenie>(zdarzenies),
-                        katalogi = katalogs.ToDictionary(k => k.id, k => k)
-                    };
-                    dr.SetDataContext(dc);
-                    break;
+                Console.WriteLine("Jaka czynnosc chcesz wykonac\n1) zapis do pliku (txt)\n2) odczyt z pliku (txt)\n3) zapis do pliku (json)\n4) odczyt z pliku (json)\n\nKazdy inny znak zakonczy dzialanie programu");
+                type = Console.ReadLine();
+                Console.WriteLine(type);
+                Console.WriteLine("Podaj nazwe pliku : ");
+                string path = Console.ReadLine();
+
+                switch (type)
+                {
+                    case "1":
+                        Writing.WriteKatalogsToFile(dr.GetAllKatalog(), path + "1.txt", iDGenerator);
+                        Writing.WriteOpisStanusToFile(dr.GetAllOpisStanu(), path + "2.txt", iDGenerator);
+                        Writing.WriteWykazsToFile(dr.GetAllWykaz(), path + "3.txt", iDGenerator);
+                        Writing.WriteZdarzeniesToFile(dr.GetAllZdarzenie(), path + "4.txt", iDGenerator);
+                        break;
+                    case "2":
+                        List<Katalog> katalogs = (List<Katalog>)reading.ReadKatalogsFromFile(path + "1.txt");
+                        List<OpisStanu> opisStanus = (List<OpisStanu>)reading.ReadOpisStanusFromFile(path + "2.txt");
+                        List<Wykaz> wykazs = (List<Wykaz>)reading.ReadWykazsFromFile(path + "3.txt");
+                        List<Zdarzenie> zdarzenies = (List<Zdarzenie>)reading.ReadZdarzeniesFromFile(path + "4.txt");
+                        DataContext dc = new DataContext
+                        {
+                            opisyStanu = opisStanus,
+                            wykazy = wykazs,
+                            zdarzenia = new ObservableCollection<Zdarzenie>(zdarzenies),
+                            katalogi = katalogs.ToDictionary(k => k.id, k => k)
+                        };
+                        dr.SetDataContext(dc);
+                        break;
+                    case "3":
+                        Writing.WriteCollectionToJSON<Katalog>(dr.GetAllKatalog(), path + "1.json");
+                        Writing.WriteCollectionToJSON<OpisStanu>(dr.GetAllOpisStanu(), path + "2.json");
+                        Writing.WriteCollectionToJSON<Wykaz>(dr.GetAllWykaz(), path + "3.json");
+                        Writing.WriteCollectionToJSON<Zdarzenie>(dr.GetAllZdarzenie(), path + "4.json");
+                        break;
+                    case "4":
+                        List<Katalog> katalogsJSON = (List<Katalog>)Reading.ReadCollectionFromJSON<Katalog>(path + "1.json");
+                        List<OpisStanu> opisStanusJSON = (List<OpisStanu>)Reading.ReadCollectionFromJSON<OpisStanu>(path + "2.json");
+                        List<Wykaz> wykazsJSON = (List<Wykaz>)Reading.ReadCollectionFromJSON<Wykaz>(path + "3.json");
+                        List<Zdarzenie> zdarzeniesJSON = (List<Zdarzenie>)Reading.ReadCollectionFromJSON<Zdarzenie>(path + "4.json");
+                        DataContext dcJSON = new DataContext
+                        {
+                            opisyStanu = opisStanusJSON,
+                            wykazy = wykazsJSON,
+                            zdarzenia = new ObservableCollection<Zdarzenie>(zdarzeniesJSON),
+                            katalogi = katalogsJSON.ToDictionary(k => k.id, k => k)
+                        };
+                        dr.SetDataContext(dcJSON);
+                        break;
+                }
             }
         }
     }
