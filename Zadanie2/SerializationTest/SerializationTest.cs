14b728c105d42c49d284269e677d80a934cd1c0f
Paweł Białek <216723@edu.p.lodz.pl>
2019-12-01 18:52:59 +0100

    Wyczyszczenie testow

--- Zadanie2/SerializationTest/SerializationTest.cs
+++ Zadanie2/SerializationTest/SerializationTest.cs
@@ -11,13 +11,6 @@ namespace Zadanie2Test
         [TestMethod]
         public void JsonFormatterTest()
         {
-            /*Class1 class1 = new Class1();
-            JsonFormatter jsonFormatter = new JsonFormatter();
-            jsonFormatter.Serialize(new FileStream("plik1.json", FileMode.Create) ,class1);
-            Class1 class12 = new Class1();
-            jsonFormatter.Deserialize(new FileStream("plik1.json", FileMode.Open));
-
-            Assert.AreEqual<Class1>(class1, class12);*/
             Class1 class1 = new Class1(2.5f, DateTime.Now, "klasa 1");
             Class2 class2 = new Class2(7.5f, DateTime.Now.AddDays(5), "klasa 2");
             Class3 class3 = new Class3(12.2f, DateTime.Now.AddMonths(2), "klasa 3");
