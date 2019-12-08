6ec56caa2b93c558b159da7f2fb448f8d1199932
Paweł Białek <216723@edu.p.lodz.pl>
2019-12-08 16:52:42 +0100

    Zaczęcie implementacji zadania 3

---
+++ Zadanie3/Zadanie3/LINQ_tools.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    class LINQ_tools
+    {
+        public static List<Product> GetProductsByName(string namePart)
+        {
+            using (CatalogDataContext dc = new CatalogDataContext())
+            {
+                Table<Product> productsTable = dc.GetTable<Product>();
+                List<Product> products = (from product in productsTable
+                                          where product.Name.Contains(namePart)
+                                          select product).ToList();
+
+                return products;
+            }
+        }
+    }
+}
