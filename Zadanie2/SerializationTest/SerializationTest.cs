5e46c3065c9f1f4e59e158111fa629af41038355
Paweł Białek <216723@edu.p.lodz.pl>
2019-12-08 19:43:07 +0100

    Uproszczenie deserializacji

--- Zadanie2/SerializationTest/SerializationTest.cs
+++ Zadanie2/SerializationTest/SerializationTest.cs
@@ -68,5 +68,44 @@ namespace Zadanie2Test
             Assert.AreEqual(class1.obj.obj.date.Second, class1prim.obj.obj.date.Second);
             Assert.AreEqual(class1.obj.obj.name, class1prim.obj.obj.name);
         }
+
+        [TestMethod]
+        public void CustomFormatterTest_v2()
+        {
+            Class1 class1 = new Class1(2.5f, DateTime.Now, "klasa 1");
+            Class2 class2 = new Class2(7.5f, DateTime.Now.AddDays(5), "klasa 2");
+            Class3 class3 = new Class3(12.2f, DateTime.Now.AddMonths(2), "klasa 3");
+            Class1 class1_1 = new Class1(0.5f, DateTime.Now.AddDays(8), "klasa 1_1");
+            class1.obj = class2;
+            class2.obj = class3;
+            class3.obj = class1;
+            class1_1.obj = class2;
+
+            CustomFormatter cs = new CustomFormatter();
+            FileStream a = new FileStream("plik2.txt", FileMode.Create);
+            cs.Serialize(a, class1_1);
+            a.Flush();
+            a.Close();
+            Class1 class1_1prim = cs.Deserialize(new FileStream("plik2.txt", FileMode.Open)) as Class1;
+
+            Assert.AreEqual(class1_1.num, class1_1prim.num);
+            Assert.AreEqual(class1_1.date.Date, class1_1prim.date.Date);
+            Assert.AreEqual(class1_1.date.Hour, class1_1prim.date.Hour);
+            Assert.AreEqual(class1_1.date.Minute, class1_1prim.date.Minute);
+            Assert.AreEqual(class1_1.date.Second, class1_1prim.date.Second);
+            Assert.AreEqual(class1_1.name, class1_1prim.name);
+            Assert.AreEqual(class1_1.obj.num, class1_1prim.obj.num);
+            Assert.AreEqual(class1_1.obj.date.Date, class1_1prim.obj.date.Date);
+            Assert.AreEqual(class1_1.obj.date.Hour, class1_1prim.obj.date.Hour);
+            Assert.AreEqual(class1_1.obj.date.Minute, class1_1prim.obj.date.Minute);
+            Assert.AreEqual(class1_1.obj.date.Second, class1_1prim.obj.date.Second);
+            Assert.AreEqual(class1_1.obj.name, class1_1prim.obj.name);
+            Assert.AreEqual(class1_1.obj.obj.num, class1_1prim.obj.obj.num);
+            Assert.AreEqual(class1_1.obj.obj.date.Date, class1_1prim.obj.obj.date.Date);
+            Assert.AreEqual(class1_1.obj.obj.date.Hour, class1_1prim.obj.obj.date.Hour);
+            Assert.AreEqual(class1_1.obj.obj.date.Minute, class1_1prim.obj.obj.date.Minute);
+            Assert.AreEqual(class1_1.obj.obj.date.Second, class1_1prim.obj.obj.date.Second);
+            Assert.AreEqual(class1_1.obj.obj.name, class1_1prim.obj.obj.name);
+        }
     }
 }
