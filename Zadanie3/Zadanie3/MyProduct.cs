3b1e23478514e01e781b4df870e535118c5dc8be
Paweł Białek <216723@edu.p.lodz.pl>
2019-12-11 20:53:18 +0100

    Etap 5 dodanie klasy MyProduct, listę obiektów tej klasy, metody na niej operujace oraz testy

---
+++ Zadanie3/Zadanie3/MyProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public class MyProduct : Product
+    {
+        public MyProduct(Product product)
+        {
+            /*Name = product.Name;
+            ListPrice = product.ListPrice;
+            ProductReviews = product.ProductReviews;
+            ProductNumber = product.ProductNumber;
+            ProductSubcategory = product.ProductSubcategory;*/
+            foreach (var property in product.GetType().GetProperties())
+            {
+                if (property.CanWrite)
+                    property.SetValue(this, property.GetValue(product));
+            }
+        }
+    }
+}
