7ad83e8f15f3908676a789522e019558e801ced0
Łukasz Kostrzewa <lukko98@gmail.com>
2020-01-12 13:23:02 +0100

    Dodano dodatkowe polaczenie do bazy danych

--- Zadanie4/Model/Properties/Settings.Designer.cs
+++ Zadanie4/Model/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace Model.Properties {
                 return ((string)(this["AdventureWorks2017ConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-IA4S3R65;Initial Catalog=AdventureWorks2017;Integrated Securit" +
+            "y=True")]
+        public string AdventureWorks2017ConnectionString1 {
+            get {
+                return ((string)(this["AdventureWorks2017ConnectionString1"]));
+            }
+        }
     }
 }
