26364fcc53ac60219a35da582551d66317486d52
Paweł Białek <216723@edu.p.lodz.pl>
2020-01-12 22:42:20 +0100

    View implementation beginning

--- Zadanie4/GUI/View/ProductListView.xaml.cs
+++ Zadanie4/GUI/View/ProductListView.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GUI
+namespace GUI.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ProductListView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ProductListView : UserControl
     {
-        public MainWindow()
+        public ProductListView()
         {
             InitializeComponent();
         }
