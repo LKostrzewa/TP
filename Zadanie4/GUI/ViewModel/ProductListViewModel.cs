e63af30562c0ccddd23519ca2cbe6ec2f9948e04
Łukasz Kostrzewa <lukko98@gmail.com>
2020-01-12 21:51:17 +0100

    Poczatek implementacji ViewModel

---
+++ Zadanie4/GUI/ViewModel/ProductListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ViewModel
+{
+    class ProductListViewModel
+    {
+        private static ProductListViewModel instance = null;
+
+        public static ProductListViewModel Instance()
+        {
+            if (instance == null)
+                instance = new ProductListViewModel();
+            return instance;
+        }
+    }
+}
---
+++ Zadanie4/GUI/ViewModel/ProductViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service;
+using Model;
+using System.Windows.Input;
+using GUI.Common;
+
+namespace GUI.ViewModel
+{
+    class ProductViewModel 
+    {
+        private ProductService productService = new ProductService();
+
+        private string productName;
+        private string productNumber;
+
+        //for opening up the Edit Customer window
+        private ICommand showEditCommand;
+
+        //for adding/saving customer information
+        private ICommand updateCommand;
+
+        //for deleting a customer
+        private ICommand deleteCommand;
+
+        //for cancel an Edit
+        private ICommand cancelCommand;
+
+        private ProductViewModel originalValue;
+
+        public int ProductId
+        {
+            get;
+            set;
+        }
+
+        public string ProductName
+        {
+            get { return productName; }
+            set
+            {
+                productName = value;
+                OnPropertyChanged("ProductName");
+            }
+        }
+
+        public string ProductNumber
+        {
+            get { return productNumber; }
+            set
+            {
+                productNumber = value;
+                OnPropertyChanged("ProductNumber");
+            }
+        }
+
+        public Mode Mode
+        {
+            get;
+            set;
+        }
+
+        public ProductListViewModel Container
+        {
+            get { return ProductListViewModel.Instance(); }
+        }
+
+        private void ShowEditDialog()
+        {
+            this.Mode = ViewModel.Mode.Edit;
+            //nwm co tu :(
+            /*IModalDialog dialog = ServiceProvider.Instance.Get<IModalDialog>();
+            dialog.BindViewModel(this); //bind to this viewModel
+            dialog.ShowDialog();*/
+        }
+
+        public ICommand ShowEditCommand
+        {
+            get
+            {
+                if (showEditCommand == null)
+                {
+                    showEditCommand = new CommandBase(i => this.ShowEditDialog(), null);
+                }
+                return showEditCommand;
+            }
+        }
+
+        private IEnumerable<Product> products;
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return this.Products;
+            }
+            set
+            {
+                this.products = value;
+                this.OnPropertyChanged("Product");
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); 
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+    }
+}
